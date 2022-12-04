348f9f90f1f94ece91c4c5ba53a0c3fe361cfd8e
creator54 <hi.creator54@gmail.com>
2022-12-04 22:51:53 +0530

    add changes suggested via chat.openai.com

--- mshell.cpp
+++ mshell.cpp
@@ -1,32 +1,42 @@
-#include<iostream>
+#include <iostream>
 #include <unistd.h>
-#define in(a) std::cin>>a
-#define out(a) printf(a)
-#define cout(a) std::cout<<a
+#include <cstdlib>
+#include <string>
 
 int flag = 1;
 std::string keypress;
-void start(){
-  if(flag){
-    system("clear");
-    out("Welcome to MSHELL. Use help\n");
-  }
-  out("$ ");
+
+void start()
+{
+    if (flag)
+    {
+        system("clear");
+        std::cout << "Welcome to MSHELL. Use help\n";
+    }
+    std::cout << "$ ";
 }
 
-void repl(){
-  start();
-  flag=0;
-  getline(std::cin,keypress);
+void repl()
+{
+    start();
+    flag = 0;
+    std::getline(std::cin, keypress);
 
-  //switches cant be used for strings ;(
-  if (keypress=="help") system("less help");
-  else if (keypress=="clr") system("clear");
-  else if (keypress=="exit") exit;
-  else system(keypress.c_str()); //https://stackoverflow.com/questions/30691299/variables-in-system-call-c
-  repl();
+    //switches cant be used for strings ;(
+    if (keypress == "help")
+        system("less help");
+    else if (keypress == "clr")
+        system("clear");
+    else if (keypress == "exit")
+        exit(0);
+    else
+        system(keypress.c_str()); //https://stackoverflow.com/questions/30691299/variables-in-system-call-c
+    repl();
 }
 
-int main(){
-  repl();
+int main()
+{
+    repl();
+    return 0;
 }
+
